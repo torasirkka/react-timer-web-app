8a3191df1082213c8de993c520baee73807383f8
Tora Sirkka <tora@torasirkka.fi>
2021-09-26 12:59:15 -0700

    Initial commit

---
+++ src/App.js
@@ -0,0 +1,30 @@
+import React, { Component } from 'react';
+import logo from './logo.svg';
+import './App.css';
+
+class App extends Component
+{
+  render()
+  {
+    return (
+      <div className="App">
+        <header className="App-header">
+          <img src={logo} className="App-logo" alt="logo" />
+          <h1 className="App-title">Welcome to React</h1>
+        </header>
+        <TimerContainer />
+      </div>
+    );
+  }
+}
+
+function TimerContainer()
+{
+
+  return (
+    <p className="App-intro">
+      {Date.now()}
+    </p>
+  )
+}
+export default App;
